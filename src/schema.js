f1b5be98da6f8e2daa46f644bb5655c91104abd0
lucky-victory <blizwonder25@gmail.com>
2021-09-04 16:38:21 +0100

    updated schema.js

--- src/schema.js
+++ src/schema.js
@@ -3,21 +3,21 @@ const U = require('../utils/helpers');
  *
  * @param {Object} options - an object takes in `name` and `fields` .
  * @param {string } [options.name=defaultShema] - the name of your schema.
- * @param {Object} options.fields - an object to specify the table columns. 
+ * @param {Object} options.fields - an object to specify the table columns.
  * @returns {Object} - returns an object.
- **/
+ * */
 function Schema(options) {
   if (!U._isObj(options)) {
     throw new TypeError('Schema options must be an object');
   }
   if (!options.fields) {
-    throw new Error('fields are required')
+    throw new Error('fields are required');
   }
-  const SCHEMA_NAME = options && options.name ? options.name : options.name = 'defaultSchema';
+  const SCHEMA_NAME = options && options.name ? options.name : 'defaultSchema';
   return {
-  name: SCHEMA_NAME,
-    fields: options.fields
+    name: SCHEMA_NAME,
+    fields: options.fields,
   };
 }
 
-module.exports=Schema;
+module.exports = Schema;
