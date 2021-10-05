7e01873103b77dd3f55b7e02066a7927ce5145ec
lucky-victory <blizwonder25@gmail.com>
2021-10-05 22:06:33 +0100

    moved validateDataKeysLength function inyo it's own file

--- helpers/validators.js
+++ helpers/validators.js
@@ -59,14 +59,6 @@ function validator(fields, newData) {
 }
 
 
-function validateDataKeysLength({ dataKeys, fieldsKeys }) {
-  if (dataKeys.length > fieldsKeys.length) {
-    throw new Error(
-      "the number of data you're trying to create is more than the columns specified in your schema, you should include them in your schema first"
-    );
-  }
-}
-
 function validateKeys({ fieldsKeys, dataKeys }) {
   const ALL_KEYS = [];
   for (let k = 0; k < fieldsKeys.length; k++) {
