c8bfa2a7523b8195d327f1b5fcc6e7450ae2aba0
lucky-victory <blizwonder25@gmail.com>
2021-10-05 22:13:48 +0100

    moved validateRequired function into it's own file

---
+++ helpers/validateRequired.js
@@ -0,0 +1,27 @@
+function validateRequired({ fieldsKeys, dataValues, requiredKeys }) {
+  const REQUIRED_KEYS = Utils._findMultipleIndex(requiredKeys, true);
+
+  if (REQUIRED_KEYS.length) {
+    for (const REQUIRED of REQUIRED_KEYS) {
+      if (
+        Utils._isObj(dataValues[REQUIRED]) ||
+        Utils._isArray(dataValues[REQUIRED])
+      ) {
+        if (Utils._isEmpty(dataValues[REQUIRED])) {
+          throw new Error(
+            `you can't leave '${fieldsKeys[REQUIRED]}' object empty because it is required`
+          );
+        }
+      } else {
+        if (
+          Utils._isUndefined(dataValues[REQUIRED]) ||
+          Utils._isEmptyStr(dataValues[REQUIRED])
+        ) {
+          throw new Error(
+            `you can't leave '${fieldsKeys[REQUIRED]}' empty because it is required`
+          );
+        }
+      }
+    }
+  }
+}
