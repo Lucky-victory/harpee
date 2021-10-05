a0b2bfe62bc4427ec9678baffae74828202ae10c
lucky-victory <blizwonder25@gmail.com>
2021-10-05 22:09:39 +0100

    updated validator.js

--- helpers/validators.js
+++ helpers/validators.js
@@ -1,6 +1,9 @@
 // @ts-ignore
 const Utils = require("./utils");
 const validateTypes=require('./validateTypes');
+const validateDataKeysLength=require('./validateKeysLength');
+const validateKeys=require('./validateKeys');
+const validateRequired=require('./validateRequired');
 
 function validator(fields, newData) {
   const FIELDS_TYPES = [];
@@ -59,23 +62,6 @@ function validator(fields, newData) {
 }
 
 
-function validateKeys({ fieldsKeys, dataKeys }) {
-  const ALL_KEYS = [];
-  for (let k = 0; k < fieldsKeys.length; k++) {
-    ALL_KEYS.push(Utils._findStrInArr(fieldsKeys, dataKeys[k]));
-  }
-  const COMPARE_KEYS = Utils._findMultipleIndex(ALL_KEYS, false);
-  if (COMPARE_KEYS.length) {
-    for (let key = 0; key < COMPARE_KEYS.length; key++) {
-      if (Utils._isUndefined(dataKeys[COMPARE_KEYS[key]])) {
-        throw new Error(
-          "you are trying to create less columns than your schema, you should exclude them in your schema or add a placeholder for it. ex: an empty string"
-        );
-      }
-      throw new Error(dataKeys[COMPARE_KEYS[key]] + " is not in your schema");
-    }
-  }
-}
 
 function validateRequired({ fieldsKeys, dataValues, requiredKeys }) {
   const REQUIRED_KEYS = Utils._findMultipleIndex(requiredKeys, true);
