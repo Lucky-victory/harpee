b435a2e1e8cd32b510a9baa4ce1bec9fc9eab133
lucky-victory <blizwonder25@gmail.com>
2021-10-23 21:16:05 +0100

    updated index.js

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-exports.connect = require('./src/connect');
-exports.Schema = require('./src/schema');
-exports.Model = require('./src/model');
+import connect from './src/connect';
+import Schema from './src/schema';
+import Model from './src/model';
+export default {
+  connect,
+  Schema,
+  Model
+}
+module.exports={
+connect :require('./src/connect'),
+Schema :require('./src/schema'),
+Model :require('./src/model')
+  
+}
