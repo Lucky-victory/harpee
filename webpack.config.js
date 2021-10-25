e19ff4737e7258be8bbd1cf57dd88a89be99e01e
lucky-victory <blizwonder25@gmail.com>
2021-10-25 11:01:16 +0100

    updated webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,11 @@
 const path=require('path')
 module.exports = {
+  mode:'production',
   entry:'./index.js',
   output: {
     path:path.resolve(__dirname,'dist'),
-    library: {
-      name: 'harpee',
-      type: 'umd',
-      
-    },
+    libraryTarget:'umd',
+      library: 'harpee',
+      umdNamedDefine:true
   },
 };
