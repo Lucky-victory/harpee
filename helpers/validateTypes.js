61b3645136603b9be209f3c672f7459f9238b4f3
lucky-victory <blizwonder25@gmail.com>
2022-01-05 21:11:15 +0100

    updated validateType.js

--- helpers/validateTypes.js
+++ helpers/validateTypes.js
@@ -2,12 +2,12 @@ function validateTypes({ types, dataTypes, dataKeys, fieldsType }) {
   const TYPES_INDEX = utils.findMultipleIndex(types, false);
 
   if (TYPES_INDEX.length) {
-    for (const NOT_SAME of TYPES_INDEX) {
-      const DATA_VALUE_TYPE = utils.getType(dataTypes[NOT_SAME]);
+    for (const INDEX of TYPES_INDEX) {
+      const DATA_VALUE_TYPE = utils.getType(dataTypes[INDEX]);
 
-      const DATA_KEY = dataKeys[NOT_SAME];
+      const DATA_KEY = dataKeys[INDEX];
 
-      const FIELD_VALUE_TYPE = utils.getType(fieldsType[NOT_SAME]);
+      const FIELD_VALUE_TYPE = utils.getType(fieldsType[INDEX]);
 
       throw new Error(
         `you are trying to assign '${DATA_VALUE_TYPE}', to '${DATA_KEY}', that has a data type of ' ${FIELD_VALUE_TYPE} '`
