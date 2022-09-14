53e442380d47b8966a52081d3dac4048b5c2a3d0
Lucky-victory <blizwonder25@gmail.com>
2022-09-14 10:49:14 +0100

    updating utils

---
+++ lib/core/sql-handler.ts
@@ -0,0 +1,302 @@
+import util from "../helpers/utils";
+
+export default class SqlHandler {
+    protected query: string = "";
+    constructor() {
+        this.query = ``;
+    }
+    /**
+     * @param {Array<string>} columns - an array of column names.
+     * @param {boolean} [distinct=false] - whether to include 'DISTINCT' keyword, default is `false`.
+     */
+    select(columns: string[], distinct: boolean = false) {
+        this.query += columns
+            ? ` SELECT ${distinct ? " DISTINCT " : ""}${columns.join(",")}`
+            : "";
+        return this;
+    }
+
+    selectCount(column: string) {
+        this.query += column ? ` SELECT COUNT(\`${column}\`)` : "";
+        return this;
+    }
+
+    selectAvg(column: string) {
+        this.query += column ? ` SELECT AVG(\`${column}\`)` : "";
+        return this;
+    }
+
+    selectSum(column: string) {
+        this.query += column ? ` SELECT SUM(\`${column}\`)` : "";
+        return this;
+    }
+    /**
+     * @param {string} schema
+     * @param {string} table
+     */
+    from(schema: string, table: string) {
+        this.query += table ? ` FROM ${schema}.\`${table}\`` : "";
+        return this;
+    }
+    as(newName: string) {
+        this.query += newName ? ` AS \`${newName}\`` : "";
+        return this;
+    }
+    /**
+     * @param {number} limit - specifying the max records.
+     *
+     * */
+    limit(limit: string) {
+        this.query += limit ? ` LIMIT ${limit}` : "";
+        return this;
+    }
+
+    /**
+     *
+     * @param {number} offset - the number of rows to skip.
+     */
+    offset(offset) {
+        this.query += offset ? ` OFFSET ${offset}` : "";
+        return this;
+    }
+    /**
+     * @param {number} offset - the number of rows to skip.
+     */
+    offsetRows(offset) {
+        this.query += offset ? ` OFFSET ${offset} ROWS` : "";
+        return this;
+    }
+    /**
+     * @param {number} rows - the number of rows to fetch.
+     */
+    fetchNextRows(rows) {
+        this.query += rows ? ` FETCH NEXT ${rows} ROWS ONLY` : "";
+        return this;
+    }
+    /**
+     * @param {string[]} columns
+     * */
+    orderBy(columns) {
+        this.query += columns ? ` ORDER BY  ${columns.join(",")}` : "";
+        return this;
+    }
+    /**
+     * @param {('DESC'|'ASC')} order
+     * */
+    order(order) {
+        this.query += order ? ` ${order}` : "";
+        return this;
+    }
+
+    delete() {
+        this.query += " DELETE";
+        return this;
+    }
+    /**
+     * @param {string} schema
+     * @param {string} table
+     */
+
+    update(schema, table) {
+        this.query += schema && table ? ` UPDATE ${schema}.\`${table}\`` : "";
+        return this;
+    }
+    /**
+     * @param {Object} options
+     * @param {string} options.schema
+     * @param {string} options.table
+     * @param {Object[]} options.records - an array of objects
+     */
+
+    insertInto(options) {
+        const { schema, table, records } = options;
+        if (Array.isArray(records)) {
+            const { keys, values } = records.reduce(
+                (accum, item) => {
+                    if (util.isObject(item)) {
+                        for (let prop in item) {
+                            accum.keys.push(prop);
+                            accum.values.push(item[prop]);
+                        }
+                    }
+                    return accum;
+                },
+                { keys: [], values: [] }
+            );
+
+            this.query += ` INSERT INTO ${schema}.\`${table}\` (${keys.join(
+                ","
+            )}) VALUES("${values.join('","')}")`;
+        } else {
+            this.query += "";
+        }
+
+        return this;
+    }
+    /**
+     * @param {Object[]} records - an array of objects
+     */
+    set(records) {
+        if (Array.isArray(records)) {
+            const keysAndValues = records.reduce((accum, item) => {
+                if (util.isObject(item)) {
+                    for (let prop in item) {
+                        accum.push(`\`${prop}\`="${item[prop]}"`);
+                    }
+                }
+                return accum;
+            }, []);
+            this.query += ` SET ${keysAndValues.join(",")}`;
+        } else {
+            this.query += "";
+        }
+
+        return this;
+    }
+    /**
+     * @param {string} condition
+     * */
+    where(condition) {
+        this.query += condition ? ` WHERE ${condition}` : "";
+        return this;
+    }
+    /**
+     * @param {string} condition
+     * */
+    whereNot(condition) {
+        this.query += condition ? ` WHERE NOT ${condition}` : "";
+        return this;
+    }
+    /**
+     * @param {(string | number)} val
+     * */
+    equalTo(val) {
+        this.query += val ? ` ='${val}'` : "";
+        return this;
+    }
+    /**
+     * @param {string} table
+     * @param {string} column
+     *
+     * */
+    isEqual(table, column) {
+        this.query += table && column ? `=${table}.\`${column}\`` : "";
+        return this;
+    }
+    /**
+     * @param {(number[]|string[])} values
+     *
+     */
+    in(values) {
+        this.query += values ? ` IN ("${values.join('","')}")` : "";
+        return this;
+    }
+    /**
+     * @param {string} pattern
+     *
+     */
+    like(pattern) {
+        this.query += values ? ` LIKE '${pattern}'` : "";
+        return this;
+    }
+
+    /**
+     * @param {string} table
+     * @param {string} column
+     */
+    on(table, column) {
+        this.query += table && column ? ` ON ${table}.\`${column}\`` : "";
+        return this;
+    }
+
+    isNotNull() {
+        this.query += ` IS NOT NULL`;
+        return this;
+    }
+    isNull() {
+        this.query += ` IS NULL`;
+        return this;
+    }
+    /**
+     * @param {number} value
+     */
+    greaterThan(value) {
+        this.query += value ? ` > ${value}` : "";
+        return this;
+    }
+    /**
+     * @param {number} value
+     */
+    lessThan(value) {
+        this.query += value ? ` < ${value}` : "";
+        return this;
+    }
+    /**
+     * @param {(string|number)} val
+     * */
+    between(val) {
+        this.query += val ? ` BETWEEN ${val}` : "";
+        return this;
+    }
+    /**
+     * @param {(string|number)} condition
+     */
+    and(condition) {
+        this.query += condition ? ` AND ${condition}` : "";
+        return this;
+    }
+    /**
+     * @param {(string|number)} condition
+     */
+    or(condition) {
+        this.query += condition ? ` OR  ${condition}` : "";
+        return this;
+    }
+    /**
+     * @param {string} schema - the schema name
+     * @param {string} table - the table name
+     */
+    crossJoin(schema, table) {
+        this.query +=
+            schema && table ? ` CROSS JOIN ${schema}.\`${table}\`` : "";
+        return this;
+    }
+    /**
+     * @param {string} schema - the schema name
+     * @param {string} table - the table name
+     */
+    fullOuterJoin(schema, table) {
+        this.query +=
+            schema && table ? ` FULL OUTER JOIN ${schema}.\`${table}\`` : "";
+        return this;
+    }
+    /**
+     * @param {string} schema - the schema name
+     * @param {string} table - the table name
+     */
+    innerJoin(schema, table) {
+        this.query +=
+            schema && table ? ` INNER JOIN ${schema}.\`${table}\`` : "";
+        return this;
+    }
+    /**
+     * @param {string} schema - the schema name
+     * @param {string} table - the table name
+     */
+    leftOuterJoin(schema, table) {
+        this.query +=
+            schema && table ? ` LEFT OUTER JOIN ${schema}.\`${table}\`` : "";
+        return this;
+    }
+    /**
+     * @param {string} schema - the schema name
+     * @param {string} table - the table name
+     */
+    rightOuterJoin(schema, table) {
+        this.query +=
+            schema && table ? ` RIGHT OUTER JOIN ${schema}.\`${table}\`` : "";
+        return this;
+    }
+}
+
+module.exports = SqlHandler;
--- lib/helpers/utils.ts
+++ lib/helpers/utils.ts
@@ -1,117 +1,118 @@
-const util = {
+import isEmpty from "just-is-empty";
+import has from "just-has";
+import omit from "just-omit";
+import pick from "just-pick";
+import safeGet from "just-safe-get";
+import safeSet from "just-safe-set";
+import typeOf from "just-typeof";
+import reduce from "just-reduce-object";
+export default class Utils {
     /** Checks if the value is an Object.
-     * @param { * } obj - the value to be checked
+     * @param { * } val- the value to be checked
      * */
 
-    isObject: function (obj) {
-        return Object.prototype.toString.call(obj) === "[object Object]";
-    },
+    static isObject(val: unknown) {
+        return typeOf(val) === "object";
+    }
     /** Checks if the value is an Array.
-     * @param { * } arr - the value to be checked
+     * @param { * } val - the value to be checked
      * */
 
-    isArray: function (arr) {
-        return Array.isArray(arr);
-    },
+    static isArray(val: unknown) {
+        return typeOf(val) === "array";
+    }
     /** Checks if the value is a String.
-     * @param { * } str - the value to be checked
+     * @param { * } val - the value to be checked
      * */
 
-    isString: function (str) {
-        return Object.prototype.toString.call(str) === "[object String]";
-    },
+    static isString(val: unknown) {
+        return typeOf(val) === "string";
+    }
     /** Checks if the value is a Number.
-     * @param { * } num - the value to be checked
+     * @param { * } val - the value to be checked
      * */
 
-    isNumber: function (num) {
-        return Object.prototype.toString.call(num) === "[object Number]";
-    },
+    static isNumber(val: unknown) {
+        return typeOf(val) === "number";
+    }
     /** Checks if the value is a Boolean.
-     * @param { * } bool - the value to be checked
+     * @param { * } val - the value to be checked
      * */
 
-    isBoolean: function (bool) {
-        return Object.prototype.toString.call(bool) === "[object Boolean]";
-    },
+    static isBoolean(val: unknown) {
+        return typeOf(val) === "boolean";
+    }
     /** Checks if the value is a Function.
      * @param { * } val - the value to be checked
      * */
 
-    isFunction: function (val) {
-        return Object.prototype.toString.call(val) === "[object Function]";
-    },
+    static isFunction(val: unknown) {
+        return typeOf(val) === "function";
+    }
     /** Checks if the value is a Undefined.
      * @param { * } val - the value to be checked
      * */
 
-    isUndefined: function (val) {
-        return (
-            Object.prototype.toString.call(val) === "[object Undefined]" ||
-            typeof val === "undefined"
-        );
-    },
-    /** Checks if the value is a Null.
+    static isUndefined(val: unknown) {
+        return typeOf(val) === "undefined";
+    }
+    /** Checks if the value is Null.
      * @param { * } val - the value to be checked
      * */
 
-    isNull: function (val) {
-        return Object.prototype.toString.call(val) === "[object Null]";
-    },
+    static isNull(val: unknown): boolean {
+        return typeOf(val) === "null";
+    }
     /** Checks if the value is a Date.
-     * @param { * } date - the value to be checked
+     * @param { * } val - the value to be checked
      * */
-    isDate: function (date) {
-        return Object.prototype.toString.call(date) === "[object Date]";
-    },
+    static isDate(val: unknown): boolean {
+        return typeOf(val) === "date";
+    }
     /** Evaluates two strings and checks if they have same or similar values.
      * @param { string } STR - the string to be evaluated.
      * @param { string } str - the string to find .
      *
      * */
-    itHas: function (STR, str) {
+    static stringHas(STR: string, str: string): boolean {
         return STR.toLowerCase().includes(str.toLowerCase());
-    },
+    }
     /** Evaluates two strings and checks if they have the same  values.
      * @param { string } STR - the string to be evaluated.
      * @param { string } str - the string to find .
      *
      * */
-    _sameStrStrict: function (STR, str) {
+    static _sameStrStrict(STR: string, str: string): boolean {
         return STR.toLowerCase().trim() === str.toLowerCase().trim();
-    },
-    /** Checks if an object or an array is empty.
-     * @param {(array | object)} arg - the value to be checked.
-     * */
-    isEmpty: function (arg) {
-        if (util.isArray(arg) || util.isObject(arg)) {
-            return Object.keys(arg).length === 0;
-        } else {
-            console.error("argument should be an array or an object");
-        }
-    },
+    }
+    /**
+     * Chekcs if a value is empty.
+     */
+    static isEmpty(val: CheckValue) {
+        return isEmpty(val);
+    }
     /** Checks if a string is empty.
      * @param {string} arg - the value to be checked.
      * */
-    isEmptyStr: function (arg) {
+    isEmptyStr(arg) {
         return arg === "";
-    },
+    }
     /** Turns the String to lowercase
      * @param {string} str - the string to be transformed
      */
-    toLower: function (str) {
+    toLower(str) {
         return String(str).toLowerCase();
-    },
+    }
     /** Turns the String to uppercase
      * @param {string} str - the string to be transformed
      */
-    toUpper: function (str) {
+    static toUpper(str) {
         return String(str).toUpperCase();
-    },
+    }
     /** Splits an object to an object of `keys` array and `values` array.
      * @param { object} obj - the object to be splitted.
      * */
-    splitObject: function (obj) {
+    static splitObject(obj) {
         const keys = [];
         const values = [];
         for (let key in obj) {
@@ -119,11 +120,11 @@ const util = {
             values.push(obj[key]);
         }
         return { keys, values };
-    },
+    }
     /** Splits an object to an object of `keys` sorted array and `values` sorted array .
      * @param { object} obj - the object to be splitted.
      * */
-    splitObjectSorted: function (obj) {
+    static splitObjectSorted(obj) {
         const keys = [];
         const values = [];
         Object.keys(obj)
@@ -133,39 +134,37 @@ const util = {
                 values.push(obj[key]);
             });
         return { keys, values };
-    },
+    }
     /**
-     * 
+     *
      * @param {{[key:string]:any}[]} arrayOfObj - an array of objects
-     * @param {boolean} [keys] - when true, returns object keys otherwise object values 
+     * @param {boolean} [keys] - when true, returns object keys otherwise object values
      * @returns {string[]}
      */
-    ObjectArrayToStringArray(arrayOfObj,keys=false){
-        if(!Array.isArray(arrayOfObj)){
-            return []
+    static ObjectArrayToStringArray<T extends object>(
+        arrayOfObj: T[],
+        keys = false
+    ) {
+        if (!Array.isArray(arrayOfObj)) {
+            return [];
         }
-        return arrayOfObj.reduce((accum,item)=>{
-            for(let key in item){
-                keys ? accum.push(key) : accum.push(item[key]);
-            }
-            return accum;
-        },[])
-    },
-    
+        // reduce();
+    }
+
     /** Splits a string by a seperator and returns the last string
      * @param {string} str - the string to be splitted.
      *
      */
-    getExtname: function (str) {
+    static getExtname(str: string) {
         return str.split(".").pop();
-    },
+    }
     /** Splits a string by a seperator and returns the first string
      * @param {string} str - the string to be splitted.
      *
      */
-    getFirst: function (str, seperator) {
+    static getFirst(str: string, seperator: string = ",") {
         return str.split(seperator).shift();
-    },
+    }
     /** Converts an object into an array seperated by a seperator
      * @param {object} obj - the object to be converted
      * @param {string} seperator - a string to seperate the key and values
@@ -173,67 +172,55 @@ const util = {
      *
      *
      */
-    objectToArray: function (obj, seperator = ",") {
+    static objectToArray<T extends object>(obj: T, seperator = ",") {
         const arr = [];
         for (let key in obj) {
             arr.push(`${key}${seperator}'${obj[key]}'`);
         }
         return arr;
-    },
+    }
     /** Returns the type of a value as a string.
      * @param { * } arg - the value to find the type.
      *
      * */
-    getType: function (arg) {
-        let type;
-
-        if (util.isString(arg) || arg === String) {
-            type = "String";
-        } else if (util.isObject(arg) || arg === Object) {
-            type = "Object";
-        } else if (util.isArray(arg) || arg === Array) {
-            type = "Array";
-        } else if (util.isNumber(arg) || arg === Number) {
-            type = "Number";
-        } else if (util.isBoolean(arg) || arg === Boolean) {
-            type = "Boolean";
-        } else if (util.isDate(arg) || arg === Date) {
-            type = "Date";
-        } else if (util.isNull(arg) || arg === null) {
-            type = "Null";
-        } else if (util.isUndefined(arg) || arg === undefined) {
-            type = "Undefined";
-        } else {
-            type = "Unknown";
-        }
+    static getType(arg: unknown) {
+        const type = typeOf(arg);
 
         return type;
-    },
+    }
 
-    
     /**Checks if an array includes a certain  string and returns true or false.
      * @param {string[]} arr - an array of string.
      * @param { string } str - the string to find in the array.
      */
-    findStringInArray: function (arr, str) {
-       
-        return (arr.includes(str)) 
-       
-    },
+    static findStringInArray(arr: string[], str: string): boolean {
+        return arr.includes(str);
+    }
     /** returns indexes of same value in an array.
      * @param { array } arr - the array to perform the operation on.
      * @param { * } val - the value to find in the array.
      * */
-    findMultipleIndex: function (arr, val) {
-        return (arr.reduce((accum, value, index) => {
+    static findMultipleIndex<T extends Array<string | number>>(
+        arr: T,
+        val: string | number
+    ): number[] {
+        return arr.reduce((accum, value, index) => {
             if (val === value) {
                 accum.push(index);
             }
             return accum;
-        }, [])
-)
-        
+        }, [] as number[]);
     }
-    };
+    static typeOf(obj: unknown) {
+        return typeOf(obj);
+    }
+}
 
-module.exports=util;
+export type CheckValue =
+    | string
+    | object
+    | date
+    | undefined
+    | null
+    | number
+    | boolean;
